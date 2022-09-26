6b35b309949fb619b66f57ee153833ee8008c75c
qwertyfive <39852023+qwertyfive@users.noreply.github.com>
2022-09-26 17:30:53 +0300

    commit

---
+++ My project/Assets/Core.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Core : MonoBehaviour
+{
+    [SerializeField] public Vector3 MoveVector;
+    [SerializeField] public float Speed;
+
+    [SerializeField] public Rigidbody Ball;
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
